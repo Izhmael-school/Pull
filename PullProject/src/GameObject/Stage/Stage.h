451c03e52fb7f5ed93582ca1e9f684adb78fc2a0
o-takehiro <mcd2476024@stu.o-hara.ac.jp>
2026-06-10 15:00:48 +0900

    oorui
    Add_Stage.h
    Add_Stage.cpp
    Add_StageBase.h

---
+++ PullProject/src/GameObject/Stage/Stage.h
@@ -0,0 +1,56 @@
+/*
+ *	@file	Stage.h
+ *  @author oorui
+ */
+
+#ifndef _STAGE_H_
+#define _STAGE_H_
+
+#include "StageBase.h"
+#include <string>
+#include <vector>
+#include "../GameObject.h"
+
+class Stage :public StageBase {
+private:
+	std::string modelPath;	// モデルのパス
+
+public:
+	/*
+	 *	コンストラクタ
+	 */
+	Stage();
+
+	/*
+	 *	デストラクタ
+	 */
+	virtual ~Stage() override;
+
+public:
+	/*
+	 *	ステージデータの読み込み
+	 *  @param[in]	const int モデルハンドル
+	 */
+	void SetModelHandle(const int modelHandleBase)override;
+
+	/*
+	 *	更新
+	 */
+	void Update() override;
+
+	/*
+	 *	描画
+	 */
+	void Render() override;
+
+	/*
+	 *	終了処理
+	 */
+	void Execute() override;
+
+
+
+};
+
+
+#endif // !_STAGE_H_
