b613407bfdb1dfd94f4f8adae810ea922a9a0f47
IchikawaRiku <mcd2476003@stu.o-hara.ac.jp>
2026-06-11 15:09:37 +0900

    Riku_Add

--- PullProject/src/GameObject/Character/Player/PlayerCharacter.cpp
+++ PullProject/src/GameObject/Character/Player/PlayerCharacter.cpp
@@ -7,6 +7,7 @@
 #include "../../../Manager/InputManager.h"
 #include "../../../Definition/Const/VECTORConst.h"
 #include "../../../Definition/CommonModule/MyMath.h"
+#include "../../../Manager/CameraManager.h"
 #include <DxLib.h>
 
 PlayerCharacter::PlayerCharacter()
@@ -18,4 +19,5 @@ void PlayerCharacter::Start() {
 }
 
 void PlayerCharacter::Update() {
+	auto camera = CameraManager::GetInstance().GetCamera()->GetTransform();
 }
