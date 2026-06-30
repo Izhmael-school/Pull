b6002749d8ec8ef9e0e855c1c599872a2361012c
IchikawaRiku <mcd2476003@stu.o-hara.ac.jp>
2026-06-30 15:07:13 +0900

    Riku_Fix_PlayerModel

--- PullProject/src/Manager/PlayerManager.cpp
+++ PullProject/src/Manager/PlayerManager.cpp
@@ -14,6 +14,7 @@ PlayerManager::PlayerManager()
 void PlayerManager::CreatePlayer() {
 	// プレイヤーの生成
 	int playerModelHandle = MV1LoadModel("res/Model/Player/HandlessPlayer.mv1");
+	//int playerModelHandle = MV1LoadModel("res/Model/Player/Simple Player.mv1");
 	pPlayer = std::make_shared<PlayerCharacter>(playerModelHandle, VGet(500, 600, 300));
 	pPlayer->Start();
 
