3f94758517838c7ca20267b71659a21c261f646a
o-takehiro <mcd2476024@stu.o-hara.ac.jp>
2026-07-08 10:19:29 +0900

    oorui
    Fixd_DebugScene
    Fixd_TUrret

--- PullProject/src/GameObject/Stage/Gimmick/Turret.cpp
+++ PullProject/src/GameObject/Stage/Gimmick/Turret.cpp
@@ -8,6 +8,10 @@
 #include "Manager/Stage/GimmickManager.h"
 #include "../../../Component\Collider/Collider.h"
 
+namespace {
+	constexpr const char* _FIREPOINT_NAME = "FirePoint";	// 発射位置の名前
+}
+
 /*
  *	コンストラクタ
  */
@@ -20,6 +24,11 @@ Turret::Turret(int modelHandle, VECTOR pos, VECTOR rota, Tag tag)
  */
 void Turret::Setup() {
 	GimmickObject::Setup();
+	// コライダー付与
+	// コライダーを付与
+	pCollider = std::make_unique<AABBCollider>(this, VGet(-200, -100, -200), VGet(200, 300, 200));
+	// レイヤーを設定
+	pCollider->SetLayer(ColliderLayer::Stage);
 }
 
 /*
@@ -39,5 +48,22 @@ void Turret::Render() {
 
 	// 描画
 	GimmickObject::Render();
+}
+
+/*
+ *	発射位置の取得
+ *  @param[out]	VECTORT 弾の発射位置の取得
+ */
+VECTOR Turret::GetFirePoint() const {
+	// モデルの行列を更新
+	MV1SetMatrix(modelHandle, pTransform->GetMatrix());
+	// モデル内のFirePointフレーム検索
+	int frame = MV1SearchFrame(modelHandle, _FIREPOINT_NAME);
+
+	// 座標を取得
+	VECTOR pos = MV1GetFramePosition(modelHandle, frame);
+
+	// 座標を返す
+	return pos;
 }
 
