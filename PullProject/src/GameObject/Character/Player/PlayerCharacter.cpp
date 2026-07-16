ad16955dca3877537447fa8c4a00822fdecc8f68
IchikawaRiku <mcd2476003@stu.o-hara.ac.jp>
2026-07-16 13:51:02 +0900

    Riku_Fix_Player

--- PullProject/src/GameObject/Character/Player/PlayerCharacter.cpp
+++ PullProject/src/GameObject/Character/Player/PlayerCharacter.cpp
@@ -61,6 +61,9 @@ void PlayerCharacter::Start() {
 
 	// 着地アニメーションにイベントを仕込む
 	auto landAnim = pAnimator->GetAnimation("Land");
+	landAnim->SetEvent([this]() {
+		throwAnimation = false;
+		}, pAnimator->GetTotalTime("Land"));
 	// 再生終了時待機状態に
 	landAnim->SetEvent([this]() {
 		playerState = PlayerState::Idle;
@@ -78,6 +81,9 @@ void PlayerCharacter::Start() {
 	// ウデ伸ばしアニメーションにイベントを仕込む
 	auto stanceAnim = pAnimator->GetAnimation("Stance");
 	float stanceAnimTime = pAnimator->GetTotalTime("Stance");
+	stanceAnim->SetEvent([this]() {
+		throwAnimation = false;
+		}, 0.0f);
 	// ループしないように停止
 	// (なぜか再生終了時間で0にするとバグるので-0.2fしている)
 	stanceAnim->SetEvent([this]() {
@@ -104,6 +110,9 @@ void PlayerCharacter::Start() {
 	// 再生中フラグ変更
 	throwAnim->SetEvent([this]() {
 		throwAnimation = false;
+		// ループしないように停止
+		pAnimator->ChangeSpeed("Throw", 0.0f);
+		pHands->GetAnimator()->ChangeSpeed("Throw", 0.0f);
 		}, throwAnimTime);
 }
 
