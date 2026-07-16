56aa354cfe9be03caac5b4da28817874c6136dc9
Izhmael-school <mcd2476033@stu.o-hara.ac.jp>
2026-07-16 11:42:17 +0900

    Fix

--- PullProject/src/UI/Scene/MainGameScreen.cpp
+++ PullProject/src/UI/Scene/MainGameScreen.cpp
@@ -1,5 +1,6 @@
 #include "MainGameScreen.h"
 #include "Definition/CommonModule/MyString.h"
+#include "Game/GameData.h"
 
 void MainGameScreen::Update(float deltaTime, const UIInput& input) {
 	coinCount->SetText(std::to_string(GameData::GetCoin()));
--- PullProject/src/UI/Scene/MainGameScreen.h
+++ PullProject/src/UI/Scene/MainGameScreen.h
@@ -10,17 +10,29 @@
 #include "../Button/UIButton.h"
 #include "../Image/UIImage.h"
 #include "../UIManager.h"
+#include "UI/Text/UIText.h"
 
 class MainGameScreen :public UIScreen {
+	UIText* coinCount;
+	UIText* scoreCount;
 public:
-	
+
 	void Init() override {
 		// UI表示
 		auto actionUI = CreateUIObject<UIImage>(
 			LoadGraph("res/UI/actionUI.png"),
 			Vector2(0, 0)
 		);
+
+		UITextStyle defaultStyle;
+		defaultStyle.fontSize = 50;
+		auto coinIcon = CreateUIObject<UIImage>(LoadGraph("res/Sprite/Game/CoinIcon.png"), Vector2(50, 50));
+		coinCount = CreateUIObject<UIText>("", defaultStyle, Vector2(100, 50));
+		scoreCount = CreateUIObject<UIText>("", defaultStyle, Vector2(1600, 50));
+
 	}
+
+	void Update(float deltaTime, const UIInput& input) override;
 };
 
 #endif // !_MAINGAMESCREEN_H_
